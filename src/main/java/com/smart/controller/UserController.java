8f7a6dc997814e8972de2c5f521f72d35b6ac503
Yugal-233 <yugalchaudhari233@gmail.com>
2021-03-18 20:11:32 +0530

    New Changes commit

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,20 +6,26 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -31,6 +37,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 	
 	@ModelAttribute
 	public void addCommonData(Model model, Principal principal) {
@@ -66,6 +75,7 @@ public class UserController {
 		
 		if(file.isEmpty()) {
 			System.out.println("File is empty");
+			contact.setImage("saara1.jpg");
 			
 		}else {
 			contact.setImage(file.getOriginalFilename());
@@ -86,5 +96,27 @@ public class UserController {
 
 		}
 		return "normal/add_contact";
+	}
+	
+	@GetMapping("/view-contacts/{page}")
+	public String viewContactDetails(@PathVariable("page") Integer page, Model model,Principal principal) {
+		model.addAttribute("title", "View Contacts Details");
+		String userName =  principal.getName();
+		User user = this.userRepository.getUserByUserName(userName);
+		Pageable pageable = PageRequest.of(page, 5);
+		Page<Contact> contacts=this.contactRepository.findContactsByUser(user.getId(), pageable);
+		model.addAttribute("contacts", contacts);
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", contacts.getTotalPages());
+		return "normal/view_contacts";
+	}
+	
+	@GetMapping("/{cId}/contact")
+	public String showContactDetails(@PathVariable("cId") Integer cId, Model model) {
+		
+		Optional<Contact> contactOptional = this.contactRepository.findById(cId);
+		Contact contact = contactOptional.get();
+		model.addAttribute("contact", contact);
+		return "normal/contact_details";
 	}
 }
